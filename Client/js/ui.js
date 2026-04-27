dd2ccb05755dc5281a5c57bf795bac4df6e445f8
Arpan Sinha <itsarpansinha@hotmail.com>
2026-04-27 23:07:14 +0530

    update

--- Client/js/ui.js
+++ Client/js/ui.js
@@ -6,3 +6,7 @@ document.querySelector(".join").addEventListener("click", () => {
     localStorage.setItem("action", "join");
     window.location.href = "lobby.html";
 });
+
+document.querySelector("#generate-room").addEventListener("click", () => {
+    socket.emit("create-room", ({roomId:"1",  name:"XYZ"}));
+})
